49770136e1fbd5fe89946c17a87d35716e5cd631
vaibhav patil~ <vaibhavpatil211299@gmail.com$ git config --global user.name vaibhav>
2024-09-28 12:05:31 +0530

    First Commit

---
+++ src/components/Footer.tsx
@@ -0,0 +1,16 @@
+import Image from "next/image";
+import logo from "../images/logo.png";
+
+const Footer = () => {
+  return (
+    <div className="w-full h-20 bg-amazon_light text-gray-300 flex items-center justify-center gap-4">
+      <Image className="w-24" src={logo} alt="logo" />
+      <p className="text-sm -mt-4">
+        All rights reserved{" "}
+       
+      </p>
+    </div>
+  );
+};
+
+export default Footer;
